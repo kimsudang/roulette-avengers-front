1dfc811a7ecb2b1dda3e93d2924a9f2c3acb21e2
kbsksy1221 <kbsksy1221@naver.com>
2024-04-26 08:11:43 +0900

    fix: 4

--- src/components/PostLayout/MyPost/MyPostLayout.jsx
+++ src/components/PostLayout/MyPost/MyPostLayout.jsx
@@ -1,26 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
-const MyPostLayout = ({ posts }) => {
-		
-  if (!posts || posts.length === 0) {
-    return <div className="post-container">작성한 게시글이 없습니다.</div>;
-  }
+const MyPostLayout = ({post}) => {
+	const navigate = useNavigate();
 
-  return (
-    <Link to={`/post/${posts.postId}`} key={posts.id} className="flex flex-col m-2.5 p-2.5 w-[270px] h-[300px] border border-[#d5d5d5] rounded">
-      <div className="w-full h-[200px]">
-        <img className="w-full h-[200px]" src={posts.image} alt="이미지"/> 
-      </div>
-      <div className="flex flex-col p-[5px]">
-        <div className='mt-[5px] ml-0 mb-0 mr-0 p-1 text-[#474747]'>
-          <p>제목: {posts.title}</p>
-        </div>
-        <div className="mt-[5px] ml-0 mb-0 mr-0 p-1 text-[#474747]">
-          <p>작성시간: {posts.createdTime}</p>
-        </div>
-      </div>
-    </Link>
-  );
-}
-
+	if (!post || post.length === 0) {
+		return <div className="post-container">게시글이 없습니다.</div>;
+	}
+	
+	return (
+		<div className="flex flex-col w-[270px] h-[350px] border border-custom-pink-1 p-2.5 rounded">
+			<Link to={`/post/${post.postId}`}>
+				<div className="w-full h-[200px]">
+				<img className='w-full h-[200px]' src={`data:image/png;base64,${post.imgBase64}`} alt="사진" />
+				</div>
+				<div className="mt-2.5 mb-1 w-full h-fit-content">
+					<p className="m-0 p-1 text-[#474747]">제목: {post.title}</p>
+					<div className="dropdown-container">
+						<p>작성자: {post.name}</p>
+					</div>
+				</div>
+			</Link>
+		</div>
+	);
+};
+  
 export default MyPostLayout;
