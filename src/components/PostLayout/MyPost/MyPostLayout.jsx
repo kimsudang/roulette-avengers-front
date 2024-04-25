f1b40018cf3ef9bf9ba3af247286b5730472511d
kbsksy1221 <kbsksy1221@naver.com>
2024-04-26 08:44:15 +0900

    fix: give me imgfilesssssssssss

--- src/components/PostLayout/MyPost/MyPostLayout.jsx
+++ src/components/PostLayout/MyPost/MyPostLayout.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const MyPostLayout = ({post}) => {
 	const navigate = useNavigate();
+	const imgLink = `https://k9bceeba41403a.user-app.krampoline.com/${post.imgSrc}`
 
 	if (!post || post.length === 0) {
 		return <div className="post-container">게시글이 없습니다.</div>;
@@ -12,7 +13,7 @@ const MyPostLayout = ({post}) => {
 		<div className="flex flex-col w-[270px] h-[350px] border border-custom-pink-1 p-2.5 rounded">
 			<Link to={`/post/${post.postId}`}>
 				<div className="w-full h-[200px]">
-				<img className='w-full h-[200px]' src={post.imgSrc} alt="사진" />
+				<img className='w-full h-[200px]' src=imgLink alt="사진" />
 				</div>
 				<div className="mt-2.5 mb-1 w-full h-fit-content">
 					<p className="m-0 p-1 text-[#474747]">제목: {post.title}</p>
