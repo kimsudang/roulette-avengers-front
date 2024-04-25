c01b73aa8de8c960b41736e21285af0c6b649211
kbsksy1221 <kbsksy1221@naver.com>
2024-04-25 17:57:26 +0900

    feat: 저장한 코드보기

---
+++ src/components/PostLayout/SaveCode/SaveCodeLayout.jsx
@@ -0,0 +1,57 @@
+import { Link, useNavigate } from "react-router-dom";
+import Mail from '../../assets/mail.png';
+import axios from "axios";
+
+const SaveCodeLayout = ({ code }) => {
+	const navigate = useNavigate();
+  const back = 'https://k9bceeba41403a.user-app.krampoline.com';
+	
+	if (!code || code.length === 0) {
+    return <div>작성한 게시글이 없습니다.</div>;
+  }
+	
+	const startChating = async () => {
+    const my_member_id = localStorage.getItem('member_id');
+    const chat_member_id = code.memberId;
+    const chat_message = '대화을 시작합니다.';
+    
+    try {
+      const res = await axios.post(`${back}/chat/start`, {
+        sender: my_member_id,
+        reciver: chat_member_id,
+        message: chat_message,
+      });
+      console.log('대화 시작');
+      navigate(`${back}/chat/start?memberId=${my_member_id}`);
+    } catch (error) {
+      alert('대화 요청을 실패하였습니다.');
+    }
+  };
+
+  return (
+    <div className='w-full h-[100px] border border-[#d5d5d5]' >
+      <div className="flex justify-between items-center" >
+        <div>
+            <div>
+              <div className="flex p-2">
+                <div className="p-2">
+                  <p>답변자: {code.memberName}</p>
+                </div>
+                <button onClick={startChating} className="p-1 w-[40px] h-10 border border-custom-pink-1 text-[#474747] rounded bg-custom-pink-1 hover:bg-custom-pink-3 transition-colors duration-300">
+                  <img className="w-[30px]" src={Mail} />
+                </button>
+              </div>
+            <div className="ml-2 p-2">
+              <p>작성시간: {code.createTime}</p>
+            </div>
+          </div>
+        </div>
+        <Link to={`/post/preview?postId=${code.postId}&replyId=${code.replyId}`} className="mr-2 p-2 h-10 border border-custom-pink-1 text-[#474747] rounded bg-custom-pink-1 hover:bg-custom-pink-3 transition-colors duration-300">
+          미리보기
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SaveCodeLayout; 
--- src/pages/MyPage/SaveCodeList.jsx
+++ src/pages/MyPage/SaveCodeList.jsx
@@ -1,8 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SaveCodeLayout from "../../components/PostLayout/SaveCode/SaveCodeLayout";
 
 const SaveCodeList = () => {
+  const [code, setCode] = useState('');
+  const back = 'https://k9bceeba41403a.user-app.krampoline.com/mypage/code';
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const accessToken = localStorage.getItem('access_token');
+        const response = await axios.get(`${back}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        const myCodes = response.data;
+        console.log(myCodes);
+        setCode(myCodes);
+      } catch (error) {
+        console.error('Error fetching posts: ', error);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <div>
-      저장한 코드 목록
+    <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 ml-[10%] mr-[10%] mb-10 justify-items-center'>
+      {code?.map(code => (
+        <SaveCodeLayout key={code.replyId} code={code} />
+      ))}
     </div>
   )
 }
