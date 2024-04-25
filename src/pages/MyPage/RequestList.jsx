3bf1c073e529d0fd8cc09e74117bab03f2449166
kbsksy1221 <kbsksy1221@naver.com>
2024-04-26 07:36:25 +0900

    fix: request

--- src/pages/MyPage/RequestList.jsx
+++ src/pages/MyPage/RequestList.jsx
@@ -8,27 +8,28 @@ const RequestList = () => {
 	const back = 'https://k9bceeba41403a.user-app.krampoline.com/mypage/list';
 	
 	useEffect(() => {
-  const fetchPosts = async () => {
-    try {
-      const accessToken = localStorage.getItem('access_token');
-      const response = await axios.get(`${back}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      const myPosts = response.data;
-      console.log('가져온 게시글:', myPosts); // 데이터 구조 확인
-      if (!Array.isArray(myPosts)) {
-        throw new Error('응답이 배열이 아님');
-      }
-      setPosts(myPosts);
-    } catch (error) {
-      console.error('게시글 가져오기 오류:', error);
-      setPosts([]); // 필요에 따라 posts를 재설정하거나 처리
-    }
-  };
-  fetchPosts();
-}, []);
+	  const fetchPosts = async () => {
+		try {
+		  const accessToken = localStorage.getItem('access_token');
+		// const memberId = localStorage.getItem('member_id');
+		  const response = await axios.get(`${back}`, {
+			headers: {
+			  Authorization: `Bearer ${accessToken}`,
+			},
+		  });
+		  const myPosts = response.data;
+		  console.log('가져온 게시글:', myPosts); // 데이터 구조 확인
+		  if (!Array.isArray(myPosts)) {
+			throw new Error('응답이 배열이 아님');
+		  }
+		  setPosts(myPosts);
+		} catch (error) {
+		  console.error('게시글 가져오기 오류:', error);
+		  // setPosts([]); // 필요에 따라 posts를 재설정하거나 처리
+		}
+	  };
+	  fetchPosts();
+	}, []);
 
 	return (
 		<div>
@@ -37,10 +38,10 @@ const RequestList = () => {
 				<Link to="/post/ask" className='mt-5 mb-5 mr-[160px] pt-1 pb-1 pl-3 pr-3 border border-[#ff9e9e] bg-[#FFE6E6] text-[#474747] no-underline'> 질문하기 </Link>
 			</div>
 			<div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 ml-[10%] mr-[10%] mb-10 justify-items-center'>
-				{Array.isArray(posts) && posts.map(post => (
-					<MyPostLayout key={post.postId} posts={post} />
-				))}
-			</div>
+			  {posts?.map(post => (
+				<PostLayout key={post.postId} post={post} />
+			  ))}
+        	</div>
 		</div>
 	)
 };
