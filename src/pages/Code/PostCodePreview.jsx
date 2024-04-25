9303f5e1aece98f93e8fee3b5b8088726e51a12c
kbsksy1221 <kbsksy1221@naver.com>
2024-04-25 23:17:45 +0900

    fix: postcodepreview.jsx

--- src/pages/Code/PostCodePreview.jsx
+++ src/pages/Code/PostCodePreview.jsx
@@ -19,31 +19,53 @@ const PostCodePreview = () => {
 	const redirect_uri = import.meta.env.VITE_BACK_REDIRECT_URI;
 	
 	
-	useEffect(() => {
-		const fetchcode = async () => {
-			try {
-				const access_token = localStorage.getItem('access_token');
-        const replyIdPayload = { 
-					replyId: replyIdNum
-				};
-				console.log(replyIdNum+1);
-				console.log(replyIdPayload);
-				const response = await axios.post(`${redirect_uri}/post/preview`, replyIdPayload, {
-						headers: {
-							'Content-Type': 'application/json',
-							Authorization: `Bearer ${access_token}`,
-						}
-					});
-					console.log(replyIdPayload);
-					const res = response.data;
-					setCode(res);
+	// useEffect(() => {
+	// 	const fetchcode = async () => {
+	// 		try {
+	// 			const access_token = localStorage.getItem('access_token');
+	// 			// const replyIdPayload = { 
+	// 			// 	replyId: replyIdNum
+	// 			// };
+	// 			console.log(replyIdNum+1);
+	// 			console.log(replyIdPayload);
+	// 			// const response = await axios.post(`${redirect_uri}/post/preview`, replyIdPayload, {
+	// 			const response = await axios.get(`${redirect_uri}/post/preview/${replyIdNum}`,)
+	// 					headers: {
+	// 						'Content-Type': 'application/json',
+	// 						Authorization: `Bearer ${access_token}`,
+	// 					}
+	// 				});
+	// 				console.log(replyIdPayload);
+	// 				const res = response.data;
+	// 				setCode(res);
 
-				} catch (error) {
-					console.error('Error fetching posts:', error);
-				}
-			};
-    fetchcode();
-  }, [replyIdNum]);
+	// 			} catch (error) {
+	// 				console.error('Error fetching posts:', error);
+	// 			}
+	// 		};
+	// fetchcode();
+	// }, [replyIdNum]);
+	
+	useEffect(() => {
+    const fetchcode = async () => {
+      try {
+        const access_token = localStorage.getItem('access_token');
+        const response = await axios.get(`${redirect_uri}/post/preview/${replyIdNum}`, {
+          Headers : {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${access_token}`,
+            }
+          }
+        })
+        const res = response.data;
+        setCode(res);
+      } catch (error) {
+        console.log('Error fetching codes: ', error);
+      }
+    }
+    fetchcode();  
+  }, []);
 	
   // 
   return (
