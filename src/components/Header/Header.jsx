13f3ecbd9b4012c68ae1fdffc280240fb20f9b1f
kbsksy1221 <kbsksy1221@naver.com>
2024-04-26 05:02:49 +0900

    style: css style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,27 @@
-import { useNavigate } from "react-router-dom"
-import { useRef } from "react"
-import Dropdown from './Dropdown'
-import Logo from '../../assets/roulette.png'
-import User from '../../assets/user.png'
-import Mail from '../../assets/mail.png'
-import useDetectClose from '../../hooks/useOutsideClick'
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import Dropdown from "./Dropdown";
+import Logo from "../../assets/catch_code_logo.png";
+// import User from "../../assets/user.png";
+import Mail from "../../assets/mail.png";
+import useDetectClose from "../../hooks/useOutsideClick";
 
 const Header = () => {
   const dropDownRef = useRef();
   const [isOpen, setIsOpen] = useDetectClose(dropDownRef, false);
   const navigate = useNavigate();
-	
-// 	로컬에서 token 가져옴
-	const token = localStorage.getItem('access_token');
-	const my_member_id = localStorage.getItem('member_id');
-	const profile = localStorage.getItem('profile');
+
+  // 	로컬에서 token 가져옴
+  const token = localStorage.getItem("access_token");
+  const my_member_id = localStorage.getItem("member_id");
+  const profile = localStorage.getItem("profile");
 
   const dropdownClicked = () => {
     setIsOpen(!isOpen);
   };
 
   const linkHome = () => {
-    navigate('/');
+    navigate("/");
   };
 
   const linkMail = () => {
@@ -29,76 +29,88 @@ const Header = () => {
   };
 
   return (
-    <div className="Header" style={{
-      width: "100%",
-      height: "70px",
-      backgroundColor: "#FFE6E6",
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-     }}>
-		 <button onClick={linkHome} 
-			style={{
-			  height: "80%",
-			  width: "70px",
-			  margin: "10px",
-			  border: "0",
-			backgroundColor: "transparent",
-			}}>
-			<img src={Logo} style={{
-			  height: "50px",
-			  width: "50px",
-			}}/>
-		 </button>
-		  {token && (
-		  	<div className="button" 
-				style={{
-				  height: "70px",
-				  padding: "20px",
-				  display: "flex",
-				  justifyContent: "space-between",
-				  alignItems: "center",
-				}}>
-				<button onClick={linkMail} 
-				  style={{
-					width: "50px",
-					height: "50px",
-					margin: "5px",
-					border: "0",
-				  }}>
-				  <img src={Mail} style={{
-					width: "30px",
-				  }}/>
-				</button>
-				<div className="dropdown" 
-				  ref={dropDownRef}
-				  style={{
-					position: "relative",
-					display: "inline-block",
-				  }}>
-				  <button 
-					className="Profile" 
-					onClick={dropdownClicked}
-					style={{
-					  width: "50px",
-					  height: "50px",
-					  margin: "5px",
-					  border: "0",
-					}} >
-					<img src={profile} style={{
-					  width: "50px",
-					  borderRadius: "50%",
-					  backgroundColor: "#ffffff",
-					}}/>
-				  </button>
-				  {isOpen && (
-					<Dropdown closeDrop={[isOpen, setIsOpen]}/>
-				  )}
-				</div>
-			</div>
-		  )}
+    <div
+      className="Header"
+      style={{
+        width: "100%",
+        height: "70px",
+        backgroundColor: "#FFE6E6",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <button
+        onClick={linkHome}
+        style={{
+          height: "120px",
+          width: "100px",
+          margin: "10px",
+          border: "0",
+          backgroundColor: "transparent",
+        }}
+      >
+        <img
+          src={Logo}
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
+        />
+      </button>
+      {!token && (
+        <div
+          className="button"
+          style={{
+            height: "70px",
+            padding: "20px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <button
+            onClick={linkMail}
+            style={{
+              width: "50px",
+              height: "50px",
+              margin: "5px",
+              border: "0",
+            }}
+          >
+            <img
+              src={Mail}
+              style={{
+                width: "30px",
+              }}
+            />
+          </button>
+          <div className="relative inline-block hover:block " ref={dropDownRef}>
+            <button
+              className="Profile"
+              onClick={dropdownClicked}
+              style={{
+                width: "50px",
+                height: "50px",
+                margin: "5px",
+                border: "0",
+              }}
+            >
+              <img
+                src={profile}
+                style={{
+                  width: "50px",
+                  borderRadius: "50%",
+                  backgroundColor: "#ffffff",
+                }}
+              />
+            </button>
+            {isOpen && <Dropdown closeDrop={[isOpen, setIsOpen]} />}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/MyPage/Myinfo.jsx
+++ src/pages/MyPage/Myinfo.jsx
@@ -1,107 +1,70 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import kakao from '../../assets/KakaoTalk_logo.png';
-import axios from 'axios';
+import kakao from "../../assets/KakaoTalk_logo.png";
+import axios from "axios";
 
 const Myinfo = () => {
-    const navigate = useNavigate();
-    const back_user = 'https://k9bceeba41403a.user-app.krampoline.com/mypage/member';
-    
-    const [userInfo, setUserInfo] = useState({
-        name: '',
-        email: '',
-    });
-    const [profile, setProfile] = useState('');
-    
-    useEffect(() => {
-        const token = localStorage.getItem('access_token');
-        const profile = localStorage.getItem('profile');
-        const fetchUserInfo = async () => {
-            try {
-                const res = await axios.get(`${back_user}`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                });
-                setUserInfo(res.data);
-            } catch (error) {
-                console.error('사용자 정보를 불러오는 중 오류가 발생했습니다.', error);
-                navigate('/');
-            }
-        };
-        if(token){
-            fetchUserInfo();
-        }
-        if(profile) {
-            setProfile(profile);
-        }
-    }, []);
-    
-    return (
-        <div style={{
-            width: "auto",
-            height: "100%",
-            padding: "5% 10%",
-            fontWeight: "bold"
-        }}>
-            <h2 style={{
-                fontSize: "50px",
-            }}>My Page</h2>
-            <div style={{
-                padding: "30px",
-                marginBottom: "30px",
-                display: "flex",
-            }}>
-                <div style={{
-                    padding: "10px",
-                }}>
-                    <p style={{
-                        fontSize: "20px", 
-                    }}>프로필</p>
-                    <div style={{
-                        padding: "10%",
-                        display: "flex", 
-                        alignItems: "center",
-                    }}>
-                        <img src={profile} alt="프로필 이미지" style={{
-                            width: "70px",
-                            height: "auto",
-                        }}/>
-                        <p style={{
-                            paddingTop: "10px",
-                            fontSize: "16px",
-                        }}>
-                            <span>{userInfo.name}</span>님
-                        </p>
-                    </div>
-                </div>
-                <div style={{
-                    padding: "30px",
-                    marginBottom: "30px",
-                }}>
-                    <p style={{
-                        fontSize: "20px",
-                    }}>소셜 로그인 계정</p>
-                    <div style={{
-                        padding: "10%",
-                        display: "flex",
-                        alignItems: "center",
-                    }}>
-                        <img src={kakao} style={{
-                            width: "50px",
-                            height: "auto",
-                        }}/>
-                        <p style={{
-                            paddingLeft: "25%",
-                            fontSize: "16px",
-                        }}>
-                            <span>{userInfo.email}</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
+  const navigate = useNavigate();
+  const back_user =
+    "https://k9bceeba41403a.user-app.krampoline.com/mypage/member";
+
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+  });
+  const [profile, setProfile] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    const profile = localStorage.getItem("profile");
+    const fetchUserInfo = async () => {
+      try {
+        const res = await axios.get(`${back_user}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUserInfo(res.data);
+      } catch (error) {
+        console.error("사용자 정보를 불러오는 중 오류가 발생했습니다.", error);
+        navigate("/");
+      }
+    };
+    if (token) {
+      fetchUserInfo();
+    }
+    if (profile) {
+      setProfile(profile);
+    }
+  }, []);
+
+  return (
+    <div className="absolute w-full pl-20 mt-16 font-bold">
+      <h2 className="text-5xl">My Page</h2>
+      <div className="m-9 mb-7">
+        <div className="p-3 mb-16">
+          <p className="text-3xl">프로필</p>
+          <div className="flex items-center p-2">
+            <img src={profile} alt="프로필 이미지" className="w-20 h-auto" />
+            <p className="pt-2 text-lg">
+              <span>{userInfo.name}</span>님
+            </p>
+          </div>
         </div>
-    );
+      </div>
+      <div className="m-7 mb-7">
+        <div className="p-3">
+          <p className="text-3xl">소셜 로그인 계정</p>
+          <div className="flex items-center p-7">
+            <img src={kakao} className="w-12 h-auto" />
+            <p className="text-lg pl-1/4">
+              <span>{userInfo.email}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Myinfo;
