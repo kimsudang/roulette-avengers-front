4bd1c57b807bca60f5bc21f4bd32e07312db1526
kbsksy1221 <kbsksy1221@naver.com>
2024-04-26 12:30:31 +0900

    feat: 내가 저장한 코드 보기

--- src/components/PostLayout/SaveCode/SaveCodeLayout.jsx
+++ src/components/PostLayout/SaveCode/SaveCodeLayout.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import Mail from "../../../assets/mail.png";
 
-const SaveCodeLayout = ({code}) => {
+const SaveCodeLayout = ({code, index}) => {
   const redirect_uri = import.meta.env.VITE_BACK_REDIRECT_URI;
 
 	if (!code || Object.keys(code).length === 0) {
@@ -15,33 +15,21 @@ const SaveCodeLayout = ({code}) => {
 			year: 'numeric',
 			month: 'long',
 			day: 'numeric',
-			hour: '2-digit',
-			minute: '2-digit',
 		}).format(date);
 	};
 	
 	const formattedTime = formatDate(code.create_time);
 
   return (
-    <div className=' m-4 w-full h-[100px] border border-[#d5d5d5] rounded'>
-      <div className="flex justify-between pl-10">
-        <div>
-          <div>
-            <div className="flex p-2">
-              <div className="mr-10 p-2">
-                <p>답변자: {code.member_name}</p>
-              </div>
-            </div>
-            <div className="ml-2 p-2">
-              <p>작성시간: {formattedTime}</p>
-            </div>
-          </div>
-        </div>
-        <div className="items-center">
-          <Link to={`/post/preview?postId=${code.postId}&replyId=${code.replyId}`} className="mt-7 mr-10 p-2 h-10 border border-custom-pink-1 text-[#474747] rounded bg-custom-pink-1 hover:bg-custom-pink-3 transition-colors duration-300">
-            미리보기
-          </Link>
-        </div>
+    <div className=' flex justify-around m-5 w-full h-[60px] border border-[#d5d5d5] rounded hover:bg-gray-100'>
+      <div className="flex justify-center items-center w-1/4">
+        <div className="text-lg text-[#747474]">{index + 1}</div>
+      </div>
+      <div className="w-1/4 flex justify-center items-center">
+        <div className="text-lg text-[#747474]">{code.member_Name}</div>
+      </div>
+      <div className="w-1/4 flex justify-center items-center">
+        <div className="text-lg text-[#747474]">{formattedTime}</div>
       </div>
     </div>
   )
--- src/pages/MyPage/SaveCodeList.jsx
+++ src/pages/MyPage/SaveCodeList.jsx
@@ -1,53 +1,29 @@
 import { useEffect, useState } from "react";
 import SaveCodeLayout from "../../components/PostLayout/SaveCode/SaveCodeLayout"
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const SaveCodeList = () => {
   const [codes, setCodes] = useState([]);
-	const [profile, setProfile] = useState([]);
-  const redirect_uri = import.meta.env.VITE_BACK_REDIRECT_URI;
-	const navigate = useNavigate();
-	const token = localStorage.getItem('access_token');
+	const redirect_uri = import.meta.env.VITE_BACK_REDIRECT_URI;
 	
 	useEffect(() => {
-    const fetchUserInfo = async () => {
-      if (!token) {
-        console.error("토큰이 없습니다.");
-        navigate("/"); // 토큰이 없으면 홈으로 리디렉션
-        return;
-      }
-      try {
-        const res = await axios.get(`${redirect_uri}/mypage/member`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        setProfile(res.data); // 사용자 정보 설정
-        loadSaveCodeList(); // 사용자 정보를 성공적으로 불러온 후 코드 목록 불러오기
-      } catch (error) {
-        console.error("사용자 정보를 불러오는 중 오류가 발생했습니다.", error);
-        navigate("/"); // 에러 발생 시 홈으로 리디렉션
-      }
-    };
-
-    const loadSaveCodeList = async () => {
-      try {
-        const res = await axios.get(`${redirect_uri}/mypage/code`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const filteredCodes = res.data.filter(code => code.member_name !== profile.name);
-				console.log(filteredCodes);
-        setCodes(filteredCodes);
-      } catch (error) {
-        console.error('Failed to get saved code list:', error);
-      }
-    };
-
-    fetchUserInfo();
-  }, [navigate, token, redirect_uri]); 
+		const fetchmycode = async () => {
+			try {
+				const access_token = localStorage.getItem('access_token');
+				const response = await axios.get(`${redirect_uri}/mypage/code`, {
+					headers : {
+						Authorization: `Bearer ${access_token}`,
+					}
+				});
+				const mycodes = response.data;
+				console.log('response: ',mycodes);
+				setCodes(mycodes);
+			} catch(error) {
+				console.error('Failed to fetch codes: ',error);
+			}
+		}
+		fetchmycode();
+	}, []);
 
   return (
     <div className="flex flex-col">
@@ -57,8 +33,8 @@ const SaveCodeList = () => {
       <div>
         <div className="flex justify-center pt-10 pb-10">
           <div className="flex w-1/2 h-screen border border-black rounded overflow-y-auto">
-						{Array.isArray(codes) && codes.map(code => (
-							<SaveCodeLayout code={code} />
+						{Array.isArray(codes) && codes.map((code, index) => (
+							<SaveCodeLayout key={code.codeId} code={code} index={index} />
 						))}
           </div>
         </div>
